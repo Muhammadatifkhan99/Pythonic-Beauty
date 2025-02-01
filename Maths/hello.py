edd11af093b0cd474f1c6b68b138764843254a9a
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-01 17:52:56 +0500

    comments, and maths operators

--- Maths/hello.py
+++ Maths/hello.py
@@ -3,3 +3,16 @@ print("hellooooooooooooooooooooooo")
 print(type(12));
 
 print(1 + 2.333); #in python int + float the result will be a floating point digit(answer)
+
+
+#the result of the division will be a float for integer divisoin we use two slashes
+#this in c++ will be zero while python have this
+print("This is a division to result in float",1/2);
+
+print("For integer division we have to use //",1//2);
+
+print("PEMDAS",1 + 2 * 5 / 2)
+
+print("PEMDAS",(1+2) * 5 / 2);
+
+print("The square root: ", 49 ** 0.5);
