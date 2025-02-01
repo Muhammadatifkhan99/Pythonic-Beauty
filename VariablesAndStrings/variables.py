82cfc15538221296dab688e196600ec32c757b69
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-02 00:08:18 +0500

    work with strings in python

---
+++ VariablesAndStrings/variables.py
@@ -0,0 +1,40 @@
+x = 100;
+
+khaleesi_mother_of_dragons = 1;
+
+print(khaleesi_mother_of_dragons + x);
+
+
+some_string = "8"
+print(some_string);
+
+list = [1,2,3]
+print(list);
+
+#python is dynamically typed so it allows us to change data types of the data....e.g from int to string and vice verse
+
+awesomeness = True
+print(awesomeness)
+
+awesomeness = "a dog"
+print(awesomeness)
+
+awesomeness = None #this represnet nothingness in python just the null in SQL and other languages
+print(awesomeness)
+
+awesomeness = 22/7
+print(awesomeness)
+
+#ESCAPE SEQUENCES
+
+new_line = "hello\nthere"
+print(new_line);
+
+str1 = "he said, 'I love him'"
+print(str1)
+
+str2 = 'he said, "I love him"'
+print(str2)
+
+str3 = 'he said,\'I love him\''
+print(str3);
