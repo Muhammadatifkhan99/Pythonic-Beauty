0b39aa78c596e9f0ac063ba13c87e1c46abe0ca6
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-02 19:15:23 +0500

    logical operators

---
+++ ConditionalLogic/conditionals.py
@@ -0,0 +1,49 @@
+# name = input()
+
+# if name == "Arya Stark":
+#     print("Valar Morghulis");
+# elif name == "Jon Snow":
+#     print("You know nothing")
+# else:
+#     print("Carry on")
+
+
+#Multiple Elif statements
+
+# color = input()
+
+# if color == "purple":
+#     print("Excellent Choice")
+# elif color == "teal":
+#     print("Not Bad")
+# elif color == "seafoam":
+#     print("mediocre")
+# elif color == "pure darkness":
+#     print("I like how you think")
+# else:
+#     print("I like how you think");
+
+
+
+# animal = input("Enter you favourite animal")
+
+# if(animal):
+#     print(animal + " is also my favourite")
+
+
+# num = 5
+
+# if num > 2 and num < 6:
+#     print("YOu can watch for free")
+
+#Logical OR
+
+# city = input()
+
+# if city == "los angeles" or city == "san franciscio":
+#     print ("you live in california")
+
+age = 25;
+
+if not ((age >= 2 and age <=8)or age >=65):
+    print("You pay 10 dollars as you are not a senior citizen")
