4060ebe3b1d516b52b18d74924fd5e82f83f78f4
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-02 14:47:41 +0500

    work on the basics of strings and conversion betwen them

--- VariablesAndStrings/variables.py
+++ VariablesAndStrings/variables.py
@@ -1,77 +1,87 @@
-x = 100;
+# x = 100;
 
-khaleesi_mother_of_dragons = 1;
+# khaleesi_mother_of_dragons = 1;
 
-print(khaleesi_mother_of_dragons + x);
-
-
-some_string = "8"
-print(some_string);
-
-list = [1,2,3]
-print(list);
-
-#python is dynamically typed so it allows us to change data types of the data....e.g from int to string and vice verse
-
-awesomeness = True
-print(awesomeness)
-
-awesomeness = "a dog"
-print(awesomeness)
-
-awesomeness = None #this represnet nothingness in python just the null in SQL and other languages
-print(awesomeness)
-
-awesomeness = 22/7
-print(awesomeness)
-
-#ESCAPE SEQUENCES
-
-new_line = "hello\nthere"
-print(new_line);
-
-str1 = "he said, 'I love him'"
-print(str1)
-
-str2 = 'he said, "I love him"'
-print(str2)
-
-str3 = 'he said,\'I love him\''
-print(str3);
-
-##CANCATENATION OF STRINGS
-
-string1 = "your";
-string2 = "face";
-
-print(string1 + string2)
-
-string3 = "";
-
-string3 += string1 + string2
-print(string3);
-
-x = 10
-# Formated strings in python 
-formattedString = f"I have told you {x} times not do these things";
-print(formattedString);
-
-x = 20
-stringformat = "I have told you {} times not do these things".format(x);
-
-print(stringformat);
-
-# string = "hello world"
-
-# for i in string:
-#     print(string[i])
-
-decimal = 12.232323;
-digit = int(decimal);
-print("The number as a decimal",decimal);
-print("The number as an int",digit);
+# print(khaleesi_mother_of_dragons + x);
 
 
-print(str(10));
+# some_string = "8"
+# print(some_string);
+
+# list = [1,2,3]
+# print(list);
+
+# #python is dynamically typed so it allows us to change data types of the data....e.g from int to string and vice verse
+
+# awesomeness = True
+# print(awesomeness)
+
+# awesomeness = "a dog"
+# print(awesomeness)
+
+# awesomeness = None #this represnet nothingness in python just the null in SQL and other languages
+# print(awesomeness)
+
+# awesomeness = 22/7
+# print(awesomeness)
+
+# #ESCAPE SEQUENCES
+
+# new_line = "hello\nthere"
+# print(new_line);
+
+# str1 = "he said, 'I love him'"
+# print(str1)
+
+# str2 = 'he said, "I love him"'
+# print(str2)
+
+# str3 = 'he said,\'I love him\''
+# print(str3);
+
+# ##CANCATENATION OF STRINGS
+
+# string1 = "your";
+# string2 = "face";
+
+# print(string1 + string2)
+
+# string3 = "";
+
+# string3 += string1 + string2
+# print(string3);
+
+# x = 10
+# # Formated strings in python 
+# formattedString = f"I have told you {x} times not do these things";
+# print(formattedString);
+
+# x = 20
+# stringformat = "I have told you {} times not do these things".format(x);
+
+# print(stringformat);
+
+# # string = "hello world"
+
+# # for i in string:
+# #     print(string[i])
+
+# decimal = 12.232323;
+# digit = int(decimal);
+# print("The number as a decimal",decimal);
+# print("The number as an int",digit);
+
+
+# print(str(10));
+
+
+# data = input("What is your name: ")
+# print("You entered"+ data);
+
+# print("What is you name: ")
+# dataa = input()
+# print(dataa)
+
+
 
 
