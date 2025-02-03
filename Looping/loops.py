5dd0ba2618d9a37b3905678da04b1d1091cd13fd
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-03 11:44:19 +0500

    looping in python

---
+++ Looping/loops.py
@@ -0,0 +1,69 @@
+# for x in range(1, 9):
+#     print(x)
+
+# print("\n")
+# for letter in "coffee":
+#     print(letter)
+
+
+# for x in range(7):
+#     print("Printing numbers form 1 to 7: ",x)
+
+# for x in range(1,9):
+#     print("Printing numbers form 1 to 9: ",x)
+
+# for x in range(1,9,2):
+#     print("Printing numbers form 1 to 9 stepping by 2: ",x)
+
+# for x in range(9,0,-1):
+#     print("Printing numbers form 9 to 1: ",x)
+
+
+# n = input("How many times I have to tell you: ")
+# n = int(n)
+# for x in range(n):
+#     print(f"time {x}: Clean up your room")
+
+# for x in range(1,20):
+#     if x == 4 or x == 13:
+#         print(f"{x}: is unlucky")
+#     if x % 2 == 0:
+#         print(f"{x}: is even")
+#     if x % 2 != 0:
+#         print(f"{x}: is odd")
+
+# msg = input("What is the secret password: ")
+
+# while msg != "bananas":
+#     print("WRONG")
+#     msg = input("What is the secret password: ")
+# print("Correct")
+
+# for time in range(1,11):
+#     print("\U0001f600" * time)
+
+
+# print("using while loop to print the pattern")
+# time = 1
+# while time < 11:
+#     print("\U0001f600" * time)
+#     time += 1
+
+#colt talking to his sisters
+
+# msg = input("Hey!! How its going")
+
+# while msg != "stop copying me":
+#     msg = input(f"{msg}\n")
+# print("UGH You Win")
+
+times = input("How many times do I have to tell you? ")
+times = int(times)
+for time in range(1,times):
+    print("CLEAN UP YOUR ROOM")
+    if time >= 4:
+        print("do you even listen anymore")
+        break
+
+    
+
