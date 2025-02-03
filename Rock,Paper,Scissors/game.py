10ae76cc81934487459dffcaa43068573dd30cdc
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-03 09:36:08 +0500

    version 1 of rock paper and scissors

---
+++ Rock,Paper,Scissors/game.py
@@ -0,0 +1,21 @@
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player1 = input("Player1, Please make your move: ")
+player2 = input("Player2, Please its you turn now: ")
+
+if player1 == "rock" and player2 == "scissors":
+    print("player1 wins!")
+elif player1 == "rock" and player2 == "paper":
+    print("player2 wins!")
+elif player1 == "paper" and player2 == "rock":
+    print("player1 wins!")
+elif player1 == "paper" and player2 == "scissors":
+    print("player2 wins")
+elif player1 == player2:
+    print("its a tie!")
+else: 
+    print("something went wrong")
+
+
