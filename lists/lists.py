91dd839a64799a6776fa9c8158ee957b7904d653
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-04 13:39:32 +0500

    slicing lists in python

--- lists/lists.py
+++ lists/lists.py
@@ -52,22 +52,41 @@
 #     print(f"{i}: {l1[i]}")
 #     i += 1
 
-l1 = ["atif","ali","khan","sahal","umair"]
+# l1 = ["atif","ali","khan","sahal","umair"]
 
 
-print("The last element is popped up: ",l1.pop()) # removes the last element by default and if index was passed removes the element at the index.
-i = 0
-while i<len(l1):
-    print(f"{i}: {l1[i]}")
-    i += 1
+# print("The last element is popped up: ",l1.pop()) # removes the last element by default and if index was passed removes the element at the index.
+# i = 0
+# while i<len(l1):
+#     print(f"{i}: {l1[i]}")
+#     i += 1
 
-print(l1.index("sahal"))
+# print(l1.index("sahal"))
 
-j = 0
-while j <= len(l1):
-    print(l1.index("sahal"))
-    j += 1;
+# j = 0
+# while j <= len(l1):
+#     print(l1.index("sahal"))
+#     j += 1;
 
-names = ["arya", "blue", "colt", "colt", "lena", "pablo", "selena"]
-print("I am friend with ".join(names))
+# names = ["arya", "blue", "colt", "colt", "lena", "pablo", "selena"]
+# print("I am friend with ".join(names))
+
+
+first_list = [1,2,3,4]
+
+print(first_list[3:])
+
+print(first_list[::-1])
+print(first_list[::-2])
+print(first_list[0:2])
+print(first_list[:])
+
+string = "This is fun"
+print(string[::-1])
+
+numbers = [1,2,3,4,5,6,7,8,9]
+print("Before: ",numbers)
+numbers[1:3] = ["a","b","c"]
+
+print("After: ",numbers)
 
