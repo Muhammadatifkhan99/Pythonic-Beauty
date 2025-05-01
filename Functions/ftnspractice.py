6e97bfc7ca66fef5cec275e003898a8e41411908
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-02 01:17:11 +0500

    doc string, to document funcitons

--- Functions/ftnspractice.py
+++ Functions/ftnspractice.py
@@ -31,3 +31,50 @@ def flip_coin():
         return "Head"
     else:
         return "Tails"
+    
+
+def exponent(num,power=2):
+    return num ** power
+
+print(exponent(2,3))
+print(exponent(2,3))
+print(exponent(4,4))
+
+#default parameters always needs to be in the end of the function definition other wise it will confuse python
+
+
+#KEYWORD ARGUMENTS-----useful if you know the names of the parameters
+
+squaure = exponent(power=2,num=10)
+print(squaure)
+
+
+#SCOPE--WHERE OUR VARIABLES ARE ACCESSIBLE IN THE PROGRAM
+
+# total = 0
+
+# def increment():
+#     total = total +  1
+#     return total
+
+# increment()
+
+
+# total = 0
+
+# def increment():
+#     global total
+#     total = total + 1
+#     return total
+
+# increment()
+
+
+#DOC STRING
+
+def say_hello():
+    """this function will print hello world to the consol"""
+    print("hello")
+
+print(say_hello.__doc__)
+
