9e8fda0d6d3b4fd15bfda7111d7f552cd951c814
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-05 23:17:24 +0500

    built functions in python

---
+++ Functions/ftnpart2.py
@@ -0,0 +1,68 @@
+# *args or star args--special operator that we can pass into our functions as a parameters
+# this gather all remaining arguments as tuple
+
+#to get out all the values out of the args, need to iterate over the args, its a tuple
+def sum_of_all_nums(*args):
+    sum = 0
+    for num in args:
+        sum += num
+    return sum
+
+
+print(sum_of_all_nums(2,3,4,5,3,5,4,3,2,2,4))
+
+
+
+##KWARGS
+
+
+
+#a special operator we can pass to functions
+#gather remaining keyword arguments as a dictionary, and store it in a dictionary instead of a tuple
+
+
+def fav_color(**kwargs):
+    print(kwargs)
+
+fav_color(colt="red",ruby="purple",ethel="black")
+
+
+#PARAMETER ORDERING
+
+#THE FOLLOWING ORDER SHOULD BE FOLLOWED BY PARAMETERS PASSED INTO FUNCTIONS
+
+# 1. parameters
+# 2. *args
+# 3. default parameters
+# 4. **kwargs
+
+
+#ARGUMENTS UNPACKING-----------------TUPLE UNPACKING----------------------
+
+#we can use * as the argument to a function to "unpack" values
+
+def sum_of_all_nums2(*args):
+    sum = 0
+#     for num in args:
+#         sum += num
+#     return sum
+
+
+# nums = [1,2,3,4,5]
+# #this dircelty passing the list to the function, will result in error
+# print(sum_of_all_nums2(nums))
+# #this syntax allows unpacking vlaues from a list
+# print(sum_of_all_nums2(*nums))
+
+
+#UNPACKING DICTIONARIES
+
+def display_name(first, second):
+    print(f"{first} says hello to {second}")
+
+names = {"first":"Ali","second":"Khan"}
+# print(display_name(names))
+
+display_name(**names)  #dictionaries unpacking
+
+
---
+++ Lambdas/lmdas.py
@@ -0,0 +1,55 @@
+def square(num): return num * num
+print(square(2))
+
+lambda num: num * num
+#lambas are functions that have no names.
+#smiliar to anonymous functions in JS
+
+square2 = lambda num: num * num * num
+
+print(square2(1))
+
+
+
+##MAPs
+# a standard function that accept at least two arguments a function and an iterable
+
+
+
+##FILTERS
+# there is a lambda for each value in the iterable
+
+l = [1,2,3,4,5,6]
+
+evens = list(filter(lambda x: x % 2 == 0,l))
+
+print(evens)
+
+
+names = ["lassie","colt", "rusty"]
+
+instructor = list(map(lambda name: f"Your Instructor is {name}", filter(lambda value: len(value) < 5, names)))
+print(instructor)
+
+# exact_name =[name for name in names: if len(name) < 5]
+
+
+#ANY AND ALL;;;;;;;;;;;;;;;
+#return true if all the elements of the iterable are truthy
+
+yes = all([char for char in "aeio" if char in "aeiou"])
+print(yes)
+
+#ANY
+#return true if any iterable of the collection is truthy
+
+#SORTED 
+#sort the data in ascending order by default, but does not modify the original list,tuple instead returna new one....
+nums = [1,2,3,4,1,2,3,1,3,2,4,2,4,9,7,6,4,5,6,2,2]
+
+print(sorted(nums))
+
+print(nums)
+
+
+#how to sort a list of dictionaries, for that we have supply an extra argument called key to the sort functoins
