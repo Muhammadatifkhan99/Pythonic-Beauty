8cbcdb47834b49e9c601df6f285c0f9222efbf18
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-14 00:15:52 +0500

    working with external modules in python

---
+++ modules/externalModules.py
@@ -0,0 +1,12 @@
+
+#pip is something that is used to install python pacakges
+#p
+
+
+import termcolor
+
+# print(dir(termcolor)) #shows attributes
+
+print(help(termcolor)) #shows the docuemntations of the module
+
+
