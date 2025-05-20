94df2602ccd351fbcd6f60a0970940c74a3c0534
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-05-21 00:17:07 +0500

    testing with python

---
+++ Testing/testing.py
@@ -0,0 +1,62 @@
+#Assertions
+
+#we make simple assertions with assert keyword
+#assert accepts an expression
+#return none if the expression is truthy 
+#Raised an assertion error if the expression is falsy
+#Accept an optional error message as a second argument
+
+# def addNum(x,y):
+#     assert x>0 and y>0, "Both numbers must be greater then zero"
+#     return x + y
+
+# print(addNum(1,2))
+# print(addNum(-1,-3))
+
+
+#if apython file is run with the flag of -O then assert statements will be ignored
+
+
+
+
+
+
+
+
+
+
+
+
+def addTwo(a,b):
+    """
+    >>> addTwo(2,3)
+    5
+    >>> addTwo(200,100)
+    300
+    """
+    return a+b
+
+# python3 -m doctest -v testing.py ....used to run the file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
