3203c00a94a59cbd5118808e2243456f2b2b75b4
Atif A. Khan <muhammadatifkhan.99@gmail.com>
2026-02-01 17:40:53 +0500

    functions pratice

--- Functions/ftnspractice.py
+++ Functions/ftnspractice.py
@@ -101,3 +101,105 @@ def say_hello():
 
 print(say_hello.__doc__)
 
+
+#Excercises
+
+#3.
+
+def last_element(l):
+    return l[len(l)-1]
+
+print(last_element([1,2,3,4,5,6,7,8]))
+
+
+#4.
+
+def greater_num(a,b):
+    if a > b:
+        return "First is greater"
+    elif b > a:
+        return "Second is greater"
+    else:
+        return "Both are equal"
+
+print(greater_num(5,2))
+
+
+def single_letter_count(str1,str2):
+    count = 0
+    for s in str1:
+        if s == str2:
+            count += 1
+    if count > 0:
+        return count
+    else:
+        return None
+
+print(single_letter_count("hello","l"))
+
+
+#4.count of letters as a dictionary
+
+def letter_count(s):
+    return {l:s.count(l) for l in s}
+
+print(letter_count("hello"))
+
+
+#5. is_palindrome function
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(is_palindrome("racecar"))
+print(is_palindrome("welcome"))
+
+#6. search times
+
+def search_times(lst, target):
+    count = 0
+    for l in lst:
+        if l == target:
+            count += 1
+    return count
+print(search_times([1,2,3,4,3,3,7,8,9],3))
+
+def even_mul_list(l):
+    product = 1
+    for i in l:
+        if i % 2 == 0:
+            product *= i
+    return product
+
+print(even_mul_list([1,2,3,4,5]))
+
+
+#7. capitalize the first letter
+
+def capitalize(s):
+    return s[0].upper() + s[1:]
+print(capitalize("hello"))
+
+
+
+def for_truthy_values(l):
+    lst = []
+    for i in l:
+        if i == True:
+            lst.append(i)
+    return lst
+
+print(for_truthy_values([True,False,None,0,1]))
+
+
+#8. intersection
+
+def intersection(l1,l2):
+    l3 = []
+    for i in l1:
+        if i in l2:
+            l3.append(i)
+    return l3
+
+print(intersection([1,2,3,4,5,6,7],[3,4,5,6,7]))
+
