b4bee7f562b4077a1b034e3600f23cc223e12eae
Atif A. Khan <muhammadatifkhan.99@gmail.com>
2026-02-11 21:52:49 +0500

    sorted() function used for all types of iterable in python to sort them

--- Lambdas/lmdas.py
+++ Lambdas/lmdas.py
@@ -78,3 +78,42 @@ print(nums)
 
 #how to sort a list of dictionaries, for that we have supply an extra argument called key to the sort functoins
 
+
+import sys
+
+list_Comp = sys.getsizeof([x*10 for x in range(1000)])
+gen_Exp = sys.getsizeof(x*10 for x in range(1000))
+
+print("List Comprehension size is: ",list_Comp)
+print("Generator Expression size is: ",gen_Exp)
+#gen expressions are very light weight and very useful and needed.
+
+
+
+###SORTED
+#it is different from sort, this returns a new sorted list from the items in iterable, like tuple, list etc
+print("SORTED")
+#it does not modify the original list...
+more_numbers = [6,1,8,2]
+print(sorted(more_numbers))
+print(more_numbers)
+#while sort will change the original list, only works with list
+nums = [2,3,4,5,6,7]
+nums.sort()
+print(nums)
+
+#we can specify the direction of sorting by using reverse=True
+print(sorted(nums,reverse=True))
+#accept a lambda, so we can specify the key based on which we want the arrangements to be
+#sorted works with almost any iterable
+
+users = [
+    {"username": "samuel", "tweets": ["I love cake", "I love pie"]},
+{"username": "katie", "tweets": []},
+{"username": "jeff", "tweets": [],"color":"purple"},
+{"username": "bob123", "tweets": [],"num":10,"color":"teal"},
+{"username": "doggo_luvr", "tweets": ["dogs are the best"]},
+{"username": "guitar_gal", "tweets": []}
+]
+print(sorted(users,key = lambda user: user["username"]))
+print(sorted(users,key=lambda user: len(user["tweets"]),reverse=True))
